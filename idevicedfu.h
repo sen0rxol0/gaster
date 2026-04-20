7351e7784ce117bf3f7cfce5c81cc072d7514af2
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2026-04-20 21:46:54 +0200

    Delete idevicedfu.h

--- idevicedfu.h
+++
@@ -1,8 +0,0 @@
-#ifndef IDEVICEDFU_H
-#define IDEVICEDFU_H
-
-char* idevicedfu_info(char *t);
-int idevicedfu_find();
-void idevicedfu_sendcommand(char* command);
-void idevicedfu_sendfile(const char* filepath);
-#endif
