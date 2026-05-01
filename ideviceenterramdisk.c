790f9882c887e03017810aa82abe6c8e31f05c7a
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2026-05-01 22:18:13 +0200

    Update ideviceenterramdisk.c

--- ideviceenterramdisk.c
+++ ideviceenterramdisk.c
@@ -1419,7 +1419,7 @@ static int stage_build_ramdisk(rdsk_ctx_t *ctx)
         return -1;
     }
 
-    if (shell_cmd("hdiutil resize -size 180MB '%s'", rdsk_dmg) != 0)
+    if (shell_cmd("hdiutil resize -size 208MB '%s'", rdsk_dmg) != 0)
         return -1;
 
     if (shell_cmd("hdiutil attach '%s' -mountpoint '%s'",
