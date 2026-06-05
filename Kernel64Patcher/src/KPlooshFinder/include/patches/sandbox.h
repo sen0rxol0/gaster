214cee0fa820569aa9258495134b77ccba08a7fd
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2026-06-05 21:15:13 +0200

    Updates

--- Kernel64Patcher/src/KPlooshFinder/include/patches/sandbox.h
+++ Kernel64Patcher/src/KPlooshFinder/include/patches/sandbox.h
@@ -7,6 +7,6 @@ extern uint32_t *vnode_lookup;
 extern uint32_t *vnode_put;
 extern uint32_t *vfs_context_current;
 
-void patch_sandbox_kext(void *real_buf, void *sandbox_buf, size_t sandbox_len);
+void patch_sandbox_kext(void *real_buf, void *sandbox_buf, size_t sandbox_len, bool has_protobox);
 
 #endif
